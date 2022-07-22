d97ceca64920526b545dee52fa074502da4c8c7e
miguelcs18 <miguelcs18@gmail.com>
2022-07-21 19:04:59 -0500

    Project to compare if two double numbers are equal up to three decimals

---
+++ src/DecimalComparator.java
@@ -0,0 +1,11 @@
+public class DecimalComparator {
+    public static void main (String[] args){
+
+    }
+
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2)
+    {
+
+        return true;
+    }
+}
