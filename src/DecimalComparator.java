0a3c032216a716a1db289b03033fdc1ab2b8336e
miguelcs18 <miguelcs18@gmail.com>
2022-07-21 19:19:42 -0500

    Dividing numbers to extract decimals

--- src/DecimalComparator.java
+++ src/DecimalComparator.java
@@ -1,11 +1,12 @@
 public class DecimalComparator {
     public static void main (String[] args){
-
+        areEqualByThreeDecimalPlaces(17.123,34.345);
     }
 
     public static boolean areEqualByThreeDecimalPlaces(double num1, double num2)
     {
-
+        System.out.println("Num 1= " + num1%num1);
+        System.out.println("Num 2= " + num2%num2);
         return true;
     }
 }
